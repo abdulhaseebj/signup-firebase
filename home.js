8203237f02a75d33ac9ed99c5033757e60cfd769
abdulhaseebj <abdulhaseeb4326@gmail.com>
2023-11-07 21:58:47 -0800

    changing

--- home.js
+++ home.js
@@ -54,18 +54,8 @@ async function getDataFromFirestore() {
                 <div class="card-body">
                     <p><span class="h4">Title:</span>${items.Title}</p>
                     <p><span class="h4">Description:</span>${items.Description}</p>
-                    <button type="button" id="delete" class="btn btn-danger">Delete</button>
-                    <button type="button" id="update" class="btn btn-info">Edit</button>
                 </div>
             </div>`
-
-        const del = document.querySelectorAll('#delete')
-        const upd = document.querySelectorAll('.#update')
-
-
-        del.forEach((btn) => {
-            console.log(btn);
-        })
 
 
     })
@@ -93,102 +83,3 @@ form.addEventListener('submit', async (e) => {
     discrption.value = ''
 })
 
-//  import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
-// import { auth, db } from "./config.js";
-// import { collection, addDoc, getDocs, Timestamp, query, orderBy } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
-
-
-// const form = document.querySelector('#form');
-// const title = document.querySelector('#title');
-// const description = document.querySelector('#description');
-// const card = document.querySelector('#card');
-
-
-// //user login or logout function
-
-// onAuthStateChanged(auth, (user) => {
-//     if (user) {
-//         const uid = user.uid;
-//         console.log(uid);
-//     } else {
-//         window.location = 'index.html'
-//     }
-// });
-
-
-// //logout function
-
-// const logout = document.querySelector('#logout-btn');
-
-// logout.addEventListener('click', () => {
-//     signOut(auth).then(() => {
-//         console.log('logout successfully');
-//         window.location = 'index.html'
-//     }).catch((error) => {
-//         console.log(error);
-//     });
-// })
-
-
-
-// //get data from firestore
-
-// let arr = []
-// async function getDataFromFirestore() {
-//     arr.length = 0;
-//     const q = query(collection(db, "posts"), orderBy('postDate', 'desc'));
-//     const querySnapshot = await getDocs(q);
-//     querySnapshot.forEach((doc) => {
-//         arr.push({ ...doc.data(), docId: doc.id });
-//     });
-//     console.log(arr);
-//     arr.map((item) => {
-//         card.innerHTML += `
-//         <div class="card mt-2">
-//         <div class="card-body">
-//             <p><span class="h4">Title:</span>${item.title}</p>
-//             <p><span class="h4">Description:</span>${item.description}</p>
-//             <button type="button" id="delete" class="btn btn-danger">Delete</button>
-//             <button type="button" id="update" class="btn btn-info">Edit</button>
-//         </div>
-//     </div>`
-//     })
-
-//     const del = document.querySelectorAll('#delete');
-//     const upd = document.querySelectorAll('#update');
-
-//     del.forEach((btn , index) => {
-//         btn.addEventListener('click', () => {
-//             console.log('delete called' , arr[index]);
-//         })
-//     })
-//     upd.forEach((btn , index) => {
-//         btn.addEventListener('click', () => {
-//             console.log('update called' , arr[index]);
-//         })
-//     })
-
-
-// }
-// getDataFromFirestore()
-
-
-// //post data on firestore
-
-// form.addEventListener('submit', async (event) => {
-//     event.preventDefault();
-//     card.innerHTML = ''
-//     try {
-//         const docRef = await addDoc(collection(db, "posts"), {
-//             title: title.value,
-//             description: description.value,
-//             uid: auth.currentUser.uid,
-//             postDate: Timestamp.fromDate(new Date()),
-//             like: false
-//         });
-//         console.log("Document written with ID: ", docRef.id);
-//         getDataFromFirestore()
-//     } catch (e) {
-//         console.error("Error adding document: ", e);
-//     }
-// })
